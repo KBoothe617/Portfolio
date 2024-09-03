4884b27a5148e48e77b8acdf5bc80dd25dc949eb
Kaleb Boothe <kboothe617@gmail.com>
2024-09-03 17:27:44 -0600

    AboutMe.jsx file added bio and picture

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,19 @@
+import './AboutMe.css';
+
+// AboutMe function returning a photo and description of myself
+
+export default function AboutMe() {
+    return (
+        <section className="about-me">
+            <div className="about-me__container">
+                <img className="about-me__photo" src="./Portfolio/src/assets/kaleb.jpg" alt="Kaleb Udy" />
+                <div className="about-me__text">
+                    <h2 className="about-me__title">About Me</h2>
+                    <p>My name is Kaleb Lee Boothe Udy. I am a fullstack webdevelopment student working to get my cetrificate in programing and webdevelopment. I was first introduced to the tech feild while attending Beehive Science and Technology Academy. During my time there I was on the FTC and FRC Robotics Teams, I worked on both the software and mechanical teams. </p>
+                    <p>I have experince in HTML, CSS, JavaScript, TypeScript, SQL, WebAPIs, React, NodeJS, Java, Python, and Prompt Engineering. I also have experince building and maintaining computers hardware components. </p>
+                    <p>My goal is to become a fullstack webdeveloper and help create websites for local buisnesses.</p>
+                </div>
+            </div>
+        </section>
+    );
+}
