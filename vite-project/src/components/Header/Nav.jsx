d773f87823786ddefd776050cbf096d4639af67a
Kaleb Boothe <kboothe617@gmail.com>
2024-09-03 22:41:11 -0600

    nav

--- vite-project/src/components/Header/Nav.jsx
+++ vite-project/src/components/Header/Nav.jsx
@@ -1,1 +1,44 @@
+import { NavLink } from 'react-router-dom';
+import Navbar from './Navbar';
 
+// Nav component for header
+export default function Nav() {
+    return (
+        <Navbar
+           links={[
+            <NavLink
+            key={1}
+            className='nav-link'
+            to='/'
+            style={({ isActive }) => ({color: isActive ? 'white' : 'black'})}
+            >
+                About Me
+            </NavLink>,
+            <NavLink
+            key={2}
+            className='nav-link'
+            to='/portfolio'
+            style={({ isActive }) => ({color: isActive ? 'white' : 'black'})}
+            >
+                Portfolio
+            </NavLink>,
+            <NavLink
+            key={3}
+            className='nav-link'
+            to='/contact'
+            style={({ isActive }) => ({color: isActive ? 'white' : 'black'})}
+            >
+                Contact
+            </NavLink>,
+            <NavLink
+            key={4}
+            className='nav-link'
+            to='/resume'
+            style={({ isActive }) => ({color: isActive ? 'white' : 'black'})}
+            >
+                Resume
+            </NavLink>
+           ]}
+        />
+    );
+}
