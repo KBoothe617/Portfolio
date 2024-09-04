a73f755eca2093ae5645348a63f5d03d45d3ea1e
Kaleb Boothe <kboothe617@gmail.com>
2024-09-03 23:04:42 -0600

    main imports and linking to diffrent pages

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBroswerRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './index.css';
+import './App.css';
+import App from './App.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Resume from './pages/Resume.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create a router with the App component as the root element linking to the AboutMe, Portfolio, Contact, and Resume components
+const router = createBroswerRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'resume',
+        element: <Resume />
+      }
+    ]
+  }
+]);
+
+// Render the router to the root element in the HTML file
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
